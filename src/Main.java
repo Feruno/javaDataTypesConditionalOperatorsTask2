b02d736cf05b9e32dc03ba3075063bf756d2605f
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-04-25 19:30:08 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialAccount = 150;
+        int amountAddition = 1500;
+
+        int bonus = 0;
+        int resSum = 0;
+        if(amountAddition >= 1000){
+            bonus = amountAddition / 100;
+            resSum = initialAccount + amountAddition + bonus;
+            System.out.println("Итогвый счёт  " + resSum + " бонусные рубли = " + bonus );
+        }else{
+            resSum = initialAccount + amountAddition;
+            System.out.println("Итогвый счёт  " + resSum + " бонусы рубли = " + bonus );
+        }
+
+    }
+}
