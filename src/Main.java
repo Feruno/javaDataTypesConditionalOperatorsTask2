c2239e4d792bf405a61700dc407483f532850f44
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-04-29 21:29:06 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
         int initialAccount = 150;
         int amountAddition = 1500;
-
         int bonus = 0;
         int resSum = 0;
-        if(amountAddition >= 1000){
+
+        if (amountAddition >= 1000) {
             bonus = amountAddition / 100;
             resSum = initialAccount + amountAddition + bonus;
-            System.out.println("Итогвый счёт  " + resSum + " бонусные рубли = " + bonus );
-        }else{
+            System.out.println("Итогвый счёт " + resSum + " бонусные рубли = " + bonus);
+        } else {
             resSum = initialAccount + amountAddition;
-            System.out.println("Итогвый счёт  " + resSum + " бонусы рубли = " + bonus );
+            System.out.println("Итогвый счёт " + resSum + " бонусы рубли = " + bonus);
         }
-
     }
 }
